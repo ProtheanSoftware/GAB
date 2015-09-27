38a54cbf190462edb854d1b3825d438b2eeb91e9
oskar367 <oskar.jedvert@gmail.com>
2015-09-27 14:08:20 +0200

    added more tests for databasehandler

--- app/src/androidTest/java/gab/protheansoftware/com/gab/model/MySQLDatabaseHandlerTest.java
+++ app/src/androidTest/java/gab/protheansoftware/com/gab/model/MySQLDatabaseHandlerTest.java
@@ -1,5 +1,6 @@
 package gab.protheansoftware.com.gab.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -9,9 +10,22 @@ import org.junit.Test;
  * Created by oskar on 2015-09-26.
  */
 public class MySQLDatabaseHandlerTest {
+    MySQLDatabaseHandler db;
+    @Before
+    public void initialize() {
+        db = new MySQLDatabaseHandler();
+    }
     @Test
     public void testAddUser() throws Exception {
-        MySQLDatabaseHandler db = new MySQLDatabaseHandler();
         db.addUser("asdasdww22", 8);
+    }
+    @Test
+    public void testGetMyID() throws Exception {
+        int id =  db.getMyId();
+        assert id == 2;
+    }
+    @Test
+     public void testAddLike() throws Exception {
+        db.addLike(2, "asdf");
     }
 }
