817452d87f8a0baa221201c94d8d7e78960f3439
Tbbas <covernant_4@hotmail.com>
2015-09-29 18:35:03 +0200

    Fixed base structure to accomodate new changes

---
+++ app/src/main/java/gab/protheansoftware/com/gab/constants/Constants.java
@@ -0,0 +1,11 @@
+package gab.protheansoftware.com.gab.constants;
+
+/**
+ * Contains constants for the application
+ */
+public class Constants {
+    public static final int num_of_simular_interest_for_match = 3;
+
+
+
+}
