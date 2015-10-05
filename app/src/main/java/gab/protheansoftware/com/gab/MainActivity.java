0b9dc867d1a6eb1c03bf27c561071361936634da
Tbbas <covernant_4@hotmai.com>
2015-10-05 11:10:42 +0200

    Renamed TabbedActivity to mainactivity, wrote comments, renamed objects

---
+++ app/src/main/java/gab/protheansoftware/com/gab/MainActivity.java
@@ -0,0 +1,97 @@
+package gab.protheansoftware.com.gab;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+
+import gab.protheansoftware.com.gab.adapter.TabsPagerAdapter;
+
+
+/**
+ * This is the main activity, it holds the chat, the list of matches and the matchscreen fragments.
+ * @author Tobias Allden
+ */
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    private ViewPager viewPager;
+    private TabsPagerAdapter tabsAdapter;
+    private ActionBar actionBar;
+    private ArrayList<Match> matches;
+
+
+
+    //Tab titles
+    private String[] tabs = {"Match Screen","Matches","Chat"};
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabbed);
+
+        //Initialize
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        actionBar = getActionBar();
+        tabsAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(tabsAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        //add tabs
+        for(String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name).setTabListener(this));
+        }
+
+    /**
+     * on swiping the viewpager make respective tab selected
+     * */
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+
+        //search for matches
+        tabsAdapter.hasmatches(false);
+        searchForMatches();
+
+    }
+
+
+    /**
+     * Searches the database for nearby matches
+     */
+    private void searchForMatches() {
+        //SEARCH DATABASE FOR MATCHES
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // on tab selected
+        // show respected fragment view
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+
+}
