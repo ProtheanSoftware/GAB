776c30614ba249f35aa26c0e3e5e03964255cb4a
Oscar Hall <hall.oscar@gmail.com>
2015-10-05 14:36:12 +0200

    Get data to matchscreen

--- app/src/main/java/gab/protheansoftware/com/gab/MatchesListFragment.java
+++ app/src/main/java/gab/protheansoftware/com/gab/MatchesListFragment.java
@@ -1,15 +1,9 @@
 package gab.protheansoftware.com.gab;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 
-import java.util.ArrayList;
-
-import gab.protheansoftware.com.gab.model.IDatabaseHandler;
+import gab.protheansoftware.com.gab.model.MySQLDatabaseHandler;
 import gab.protheansoftware.com.gab.model.Profile;
 import gab.protheansoftware.com.gab.adapter.MatchesListAdapter;
 
@@ -18,15 +12,26 @@ import gab.protheansoftware.com.gab.adapter.MatchesListAdapter;
  * Implement match list here
  */
 public class MatchesListFragment extends android.support.v4.app.ListFragment {
-IDatabaseHandler dbh;
+MySQLDatabaseHandler dbh;
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
-        String[] matches = {"banan","Candy"};
+
+        dbh = new MySQLDatabaseHandler();
+
+        Profile[] matches = new Profile[0];
+        try {
+            matches = (Profile[]) dbh.getMatches().toArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         ListAdapter matchesListAdapter = new MatchesListAdapter(getActivity(), matches);
+
         setListAdapter(matchesListAdapter);
     }
 
