60b7ca5547a1fdd4784e46e4f88c14b49cb6843a
Oscar Hall <hall.oscar@gmail.com>
2015-10-06 19:00:43 +0200

    OnItemClickListner matcheslist

---
+++ app/src/main/java/gab/protheansoftware/com/gab/MatchesListFragment.java
@@ -0,0 +1,53 @@
+package gab.protheansoftware.com.gab;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import gab.protheansoftware.com.gab.model.IDatabaseHandler;
+import gab.protheansoftware.com.gab.model.Profile;
+import gab.protheansoftware.com.gab.adapter.MatchesListAdapter;
+import gab.protheansoftware.com.gab.model.JdbcDatabaseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implement match list here
+ */
+public class MatchesListFragment extends android.support.v4.app.ListFragment implements AdapterView.OnItemClickListener {
+
+    private IDatabaseHandler dbh;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+        setDbh(new JdbcDatabaseHandler());
+        List<Profile> matches = new ArrayList<Profile>();
+        try {
+            matches = dbh.getMatches();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ListAdapter matchesListAdapter = new MatchesListAdapter(getActivity(), matches);
+        setListAdapter(matchesListAdapter);
+    }
+
+    public void setDbh(IDatabaseHandler dbh){
+        this.dbh = dbh;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), String.valueOf(parent.getItemAtPosition(position)), Toast.LENGTH_SHORT);
+    }
+
+
+}
