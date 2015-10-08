f1542c7ce5e260c0946a59cf3cc872c091212f54
Tbbas <covernant_4@hotmai.com>
2015-10-08 13:13:32 +0200

    Refactored package structure

--- app/src/main/java/com/protheansoftware/gab/ChatFragment.java
+++ app/src/main/java/com/protheansoftware/gab/ChatFragment.java
@@ -1,17 +1,22 @@
-package gab.protheansoftware.com.gab;
+package com.protheansoftware.gab;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SearchforMatches extends android.support.v4.app.Fragment {
 
+/**
+ * Implement a chat in this fragment
+ */
+public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_searchfor_matches,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_chat,container,false);
         return rootView;
     }
+
 }
---
+++ app/src/main/java/com/protheansoftware/gab/model/Secrets.java
@@ -0,0 +1,7 @@
+package com.protheansoftware.gab.model;
+
+public class Secrets {
+    public static final String DB_USER = "gab";
+    public static final String DB_PASSWORD = "m5aSqhAaKz6sXANe";
+    public static final String DB_IP = "83.248.219.132:3306";
+}
