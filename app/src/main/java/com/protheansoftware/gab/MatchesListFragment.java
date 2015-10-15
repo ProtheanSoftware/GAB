2e35952529ec57ee7eb394b3751a999394dc865b
Tbbas <covernant_4@hotmail.com>
2015-10-15 17:38:47 +0200

    Rewrote listview and added added a bottomsheet with removebutton

--- app/src/main/java/com/protheansoftware/gab/MatchesListFragment.java
+++ app/src/main/java/com/protheansoftware/gab/MatchesListFragment.java
@@ -1,26 +1,25 @@
 package com.protheansoftware.gab;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.protheansoftware.gab.adapter.TabsPagerAdapter;
+import com.flipboard.bottomsheet.BottomSheetLayout;
 import com.protheansoftware.gab.model.IDatabaseHandler;
 import com.protheansoftware.gab.model.Profile;
 import com.protheansoftware.gab.adapter.MatchesListAdapter;
 import com.protheansoftware.gab.model.JdbcDatabaseHandler;
-import com.protheansoftware.gab.chat.MessageService;
 import com.protheansoftware.gab.chat.MessagingFragment;
-import com.protheansoftware.gab.model.IDatabaseHandler;
-import com.protheansoftware.gab.model.Profile;
-import com.protheansoftware.gab.adapter.MatchesListAdapter;
-import com.protheansoftware.gab.model.JdbcDatabaseHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +29,17 @@ import java.util.Observable;
 /**
  * Implement match list here
  */
-public class MatchesListFragment extends android.support.v4.app.ListFragment implements AdapterView.OnItemClickListener {
+public class MatchesListFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private static final String TAG = "MatchesListFragment";
     private IDatabaseHandler dbh;
     private Main2Activity main;
+    private BottomSheetLayout bottomSheetLayout;
+    private ListView list;
+    Profile currentUser;
+    ArrayAdapter matchesListAdapter;
+
+
 
     public Observable notifier;
 
@@ -53,11 +58,44 @@ public class MatchesListFragment extends android.support.v4.app.ListFragment imp
         } catch (Exception e) {
             e.printStackTrace();
         }
+        list = (ListView)getActivity().findViewById(R.id.list);
+         matchesListAdapter = new MatchesListAdapter(getActivity(), matches);
+        list.setAdapter(matchesListAdapter);
+        list.setOnItemClickListener(this);
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
-        ListAdapter matchesListAdapter = new MatchesListAdapter(getActivity(), matches);
-        setListAdapter(matchesListAdapter);
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+                if(arg0.getItemAtPosition(pos) instanceof Profile) {
+                    currentUser = ((Profile)arg0.getItemAtPosition(pos));
+                    bottomSheetLayout = (BottomSheetLayout) getActivity().findViewById(R.id.bottomsheet);
+                    bottomSheetLayout.showWithSheetView(getActivity().getLayoutInflater().inflate(R.layout.remove_menu, bottomSheetLayout, false));
+                    ((TextView)getActivity().findViewById(R.id.deleteNameTag)).setText(currentUser.getName());
 
-        getListView().setOnItemClickListener(this);
+
+                }
+                ((Button)getActivity().findViewById(R.id.btn_remove)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //removeuser
+
+                        matchesListAdapter.notifyDataSetChanged();
+                        bottomSheetLayout.dismissSheet();
+                        currentUser = null;
+
+
+                    }
+                });
+
+                return true;
+            }
+        });
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_item_list,container,false);
+        return rootView;
     }
 
     public void setDbh(IDatabaseHandler dbh){
@@ -77,6 +115,7 @@ public class MatchesListFragment extends android.support.v4.app.ListFragment imp
 
         //Switch tab and open chat
         main.openChat();
+
     }
 
     public void setMain(Main2Activity main) {
